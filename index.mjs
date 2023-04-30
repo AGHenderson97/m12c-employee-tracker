0756bf58b088ee3a06bf2f61d8e66518ed38f01f
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-30 01:26:33 -0400

    updated MySQL connection password

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'your_mysql_password',
+  password: 'm9cpassword',
   database: 'your_database_name',
 });
 
